eabad47dc4822661350b6e04e0928d89de6739d1
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-07-26 15:13:02 -0500

    Update Servers_Hotfix Inventory.ps1

--- Servers/Servers_Hotfix Inventory.ps1
+++ Servers/Servers_Hotfix Inventory.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 ForEach ($computer in $computers) {   
   
   try   
-    {  Get-HotFix -cn $computer | Select-Object PSComputerName,HotFixID,Description,InstalledBy,InstalledOn | export-CSV C:\temp\Hotfixes\Hotfix-$computer.csv -NoTypeInformation -Encoding UTF8  }  
+    {  Get-HotFix -cn $computer | Select-Object PSComputerName,HotFixID,Description,InstalledBy,InstalledOn | export-CSV C:\temp\Hotfixes\Hotfix-Inventory.csv -Append -NoTypeInformation -Encoding UTF8  }  
   
 catch    
     {  Write-Warning "System Not reachable:$computer" }   
