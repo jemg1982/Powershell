506ba949382a2bd0f48ae924eb3c4793d56843c1
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-07-26 10:23:52 -0500

    Add files via upload

---
+++ AD_ListofHotfix.ps1
@@ -0,0 +1,14 @@
+﻿
+
+$computers = Get-Content C:\Temp\hotfix_computers.txt    
+$ErrorActionPreference = 'Stop'   
+ 
+ForEach ($computer in $computers) {   
+  
+  try   
+    {  Get-HotFix -cn $computer | Select-Object PSComputerName,HotFixID,Description,InstalledBy,InstalledOn | export-CSV C:\temp\Hotfixes\Hotfix-$computer.csv -NoTypeInformation -Encoding UTF8  }  
+  
+catch    
+    {  Write-Warning "System Not reachable:$computer" }   
+}  
+  
