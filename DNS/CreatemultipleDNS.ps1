7faaceb5b8238eca1d7113d92b01d3878dae3935
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-08-04 08:54:23 -0500

    Update CreatemultipleDNS.ps1

--- DNS/CreatemultipleDNS.ps1
+++ DNS/CreatemultipleDNS.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS 
+This script creates multple DNS records from an input CSV
+
+.DESCRIPTION
+The script takes an input CSV file and creates 
+a DNS record (A and PTR) for each entry. For the PTR, it also creates the zone if it doesnt exists. 
+
+.INPUTS
+Input CSV file with columns named Computer and IP. 
+
+.OUTPUTS
+Creates multiple DNS records. 
+
+.NOTES
+1. Please observe the construction of the PTR record to follow your enterprise strategy or best practice. 
+2. The script uses dnscmd command which is not a powershell command but a command-line interface for managing
+DNS servers. 
+#>
+
+
 $ServerName = "DNSServer.contoso.com" 
 $domain = "contoso.com" 
 
