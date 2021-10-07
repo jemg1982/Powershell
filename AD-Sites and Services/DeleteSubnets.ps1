f4547e123f9877b3de0c7ffcb204061d9e21eeec
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-10-07 16:05:56 -0500

    Create DeleteSubnets.ps1

---
+++ AD-Sites and Services/DeleteSubnets.ps1
@@ -0,0 +1,28 @@
+﻿
+<#
+.SYNOPSIS 
+This script deletes multple AD subnets
+
+.DESCRIPTION
+The script takes an input CSV file (Name) and deletes the corresponding AD subnets per each row. 
+
+.INPUTS
+Input CSV file with columns named Name
+
+.OUTPUTS
+Deletes multiple AD subnets. 
+
+.NOTES
+This was created to delete subnets for a cleanup. 
+#>
+
+Import-Module ActiveDirectory
+
+$subnets = Import-csv "C:\tools\Scripts\Subnets\subnets.csv" 
+
+ForEach ($item in $subnets) 
+{
+$Name = $item.Name
+ 
+Remove-ADReplicationSubnet -Identity $Name -Confirm:$false
+}
