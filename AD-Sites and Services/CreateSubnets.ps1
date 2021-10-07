f31a88e032d5db6294350368399e40e16fa1cae9
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-10-07 16:05:50 -0500

    Create CreateSubnets.ps1
    AD Sites and Services

---
+++ AD-Sites and Services/CreateSubnets.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS 
+This script creates multple AD subnets
+
+.DESCRIPTION
+The script takes an input CSV file (Name,Site,Description) and creates AD subnets per each row. 
+
+.INPUTS
+Input CSV file with columns named Name, Site and Description. 
+
+.OUTPUTS
+Creates multiple AD subnets. 
+
+.NOTES
+This was created to recover subnets after a cleanup. 
+#>
+
+Import-Module ActiveDirectory
+
+$subnets = Import-csv "C:\temp\subnets.csv" 
+
+ForEach ($item in $subnets) 
+{
+$Name = $item.Name
+$Site = $item.Site
+$Description = $item.Description
+
+New-ADReplicationSubnet -Name $Name -Site $Site -Description $Description
+} 
