89a020b0222165622e1d51a2358bf7e4c3f12250
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-10-07 16:06:00 -0500

    Create ReportSubnets.ps1

---
+++ AD-Sites and Services/ReportSubnets.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS 
+Creates a report of all the subnets in AD Sites and Services
+
+.DESCRIPTION
+The script lists all the subnets in AD sites and servcies and creates a report. 
+
+.INPUTS
+None
+
+.OUTPUTS
+Creates AD subnets report. 
+
+.NOTES
+
+#>
+
+
+Import-Module ActiveDirectory
+$Subnets = Get-ADReplicationSubnet -filter * -Properties * | Select Name, Site, Location, Description
+$ResultsArray = @()
+
+ForEach ($Subnet in $Subnets) {
+
+    $SiteName = ""
+    If ($Subnet.Site -ne $null) { $SiteName = $Subnet.Site.Split(',')[0].Trim('CN=') }
+    
+    $RA = New-Object PSObject
+    $RA | Add-Member -type NoteProperty -name "Name"   -Value $Subnet.Name
+    $RA | Add-Member -type NoteProperty -name "Site" -Value $SiteName
+    $RA | Add-Member -type NoteProperty -name "Description" -Value $Subnet.Description
+
+    $ResultsArray += $RA
+}
+
+$ResultsArray | Sort Subnet | Export-Csv C:\temp\AD-Subnets.csv -nti
+$Report = Get-Content c:\temp\AD-Subnets.csv
+$Report.Replace('","',",").TrimStart('"').TrimEnd('"') | Out-File c:\temp\AD-Subnets.csv -Force -Confirm:$false
