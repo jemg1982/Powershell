194bcc2533c50e9961607cf0caa5e8b7b33c59f8
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-10-08 09:33:41 -0500

    Update Extract group members (+5000).ps1
    Add dpcumentation

--- Groups/Extract group members (+5000).ps1
+++ Groups/Extract group members (+5000).ps1
@@ -1,1 +1,22 @@
-﻿(Get-ADGroup -Identity "Lotus Notes" -Properties Members).Members | Get-ADUser | Select-Object name,samAccountName | Export-Csv c:\temp\lotusnotesgroup.csv
+﻿<#
+.SYNOPSIS 
+This script creates a report of the memmbers of an AD security group over 5000 members.  
+
+.DESCRIPTION
+This script creates a report of the memmbers of an AD security group over 5000 members using a different mechanism since the regular process stops at 5000.
+
+
+.INPUTS
+Group name
+
+.OUTPUTS
+CSV file with group members
+
+.NOTES
+Improvements: 
+* Error proofing
+* Relative Output path
+#>
+
+
+(Get-ADGroup -Identity "Lotus Notes" -Properties Members).Members | Get-ADUser | Select-Object name,samAccountName | Export-Csv c:\temp\lotusnotesgroup.csv
