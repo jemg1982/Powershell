e8c01e4361b10a346c593631bb945a38812db90d
jemg1982 <87879906+jemg1982@users.noreply.github.com>
2021-10-08 09:39:06 -0500

    Update TestDNS.ps1

--- DNS/TestDNS.ps1
+++ DNS/TestDNS.ps1
@@ -2,6 +2,6 @@ $DNSServers = Get-Content -path C:\temp\dnsservers.txt
 
 Foreach($DNSServer in $DNSServers) {
 
-Test-DnsServer -IPAddress $DNSServer -Zonename "cna.com"
+Test-DnsServer -IPAddress $DNSServer -Zonename "contoso.com"
 
 }
